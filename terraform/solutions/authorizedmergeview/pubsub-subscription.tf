a3b9b2e2d905bafedd319f5d95cc1179d6ecfddd
Jason Brown <jajabro@google.com>
2026-03-12 12:19:04 +0000

    remove old implementation locations

--- terraform/solutions/authorizedmergeview/pubsub-subscription.tf
+++
@@ -1,6 +0,0 @@
-resource "google_pubsub_subscription" "pubsub_bidask_all_to_bq" {
-  name  = "pubsub_bidask_all_to_bq_mergeview"
-  topic = var.pubsub_topic_id
-
-  ack_deadline_seconds = 20
-}
--- terraform/storage.tf
+++
@@ -1,5 +0,0 @@
-resource "google_storage_bucket" "function_bucket" {
-  name                        = "${var.project_id}-function-source"
-  location                    = "US"
-  uniform_bucket_level_access = true
-}
